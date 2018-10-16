d38ace1df63f7d0bbc6096848c73c2c8befd2d8a
jfo <jfo@pfb20.cshl.edu>
2018-10-16 15:54:23 -0400

    Added PS2-11 to PS2-8

--- Python_02/PS2-8.py
+++ Python_02/PS2-8.py
@@ -5,6 +5,11 @@ variable = int(sys.argv[1])
 
 if variable > 0:
 	print("Positive")
+	if variable < 50:
+		result = int(variable)
+		result %= int(variable) 
+		if result == 0:
+			print(variable,"is an even number that is smaller than 50")
 
 elif variable == 0:
 	print(variable,"is equal to 0")
