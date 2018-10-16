6514ba6a63202180a899ed9c4ded05b542a7b77a
jfo <jfo@pfb20.cshl.edu>
2018-10-16 11:13:11 -0400

    Added command line reading

--- Python_01/PS1-2.py
+++ Python_01/PS1-2.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+import sys
 
-myname="Jannine"
-print(myname)
+# My name
+myname= sys.argv[1]
+print('My name:',myname)
+
+# Favourite colour
+favcolour= sys.argv[2]
+print('My favourite colour: ',favcolour)
+
+# Favourite activity
+favactivity= sys.argv[3]
+print('My favourite activity: ',favactivity)
+
+# Favourite animal
+favanimal= sys.argv[4]
+print('My favourite animal: ',favanimal)
 
