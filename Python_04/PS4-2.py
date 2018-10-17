f39c96f4917682832df3113a1556f2673465e92d
jfo <jfo@pfb20.cshl.edu>
2018-10-17 16:03:25 -0400

    Added PS4 problemsets

---
+++ Python_04/PS4-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+#Create and print string
+
+people = [sapiens, erectus, neanderthalensis]
+print(people)
+
+#Split the string into individual words
+
+species = people.split(',')
+print(species)
+
+
