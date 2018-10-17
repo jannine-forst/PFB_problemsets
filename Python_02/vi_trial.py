72da9fcfd029d0365ff7b6c2009d9369d2de0315
jfo <jfo@pfb20.cshl.edu>
2018-10-17 10:42:10 -0400

    Added python 3 problemset

---
+++ Python_02/vi_trial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+# My name
+myname = sys.argv[1]
+print('My name:',myname)
+
+print("\nScript has finished running!")
+
+print("\nScript has finished running!")
+print("\nScript has finished running!")
+print("\nScript has finished running!")
+#Comments here
