a82f513f534db58c1663df196ed0f5e87e88436f
jfo <jfo@pfb20.cshl.edu>
2018-10-17 13:26:28 -0400

    Added PS3-14

---
+++ Python_03/PS3-14.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+
+dna = "AGAATTC"
+print("Should be position 2, nucleotide should be A")
+
+#Find the starting nucleotide position of an EcoRI site in the DNA sequence
+
+ecoRI = "GAATTC"
+
+ecoInDNA = dna.find(ecoRI)
+print(ecoRI)
+
+
+
+#Find the ending nucleotide position
+
+
+
+#Print outusing string formatting
+
+
+
+
