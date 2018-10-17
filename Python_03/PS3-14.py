75b2f75794fd152879b8d0f6116e6ac5fd940b02
jfo <jfo@pfb20.cshl.edu>
2018-10-17 13:31:58 -0400

    Added PS3-14 edits

--- Python_03/PS3-14.py
+++ Python_03/PS3-14.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import sys
 
-dna = "AGAATTC"
-print("Should be position 2, nucleotide should be A")
+dna = "AGAATTCT"
+print("Should be position 1, nucleotide should be G")
 
 #Find the starting nucleotide position of an EcoRI site in the DNA sequence
 
 ecoRI = "GAATTC"
 
 ecoInDNA = dna.find(ecoRI)
-print(ecoRI)
-
-
+print(ecoInDNA)
+print(type(ecoInDNA))
 
 #Find the ending nucleotide position
 
