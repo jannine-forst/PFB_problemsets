9690b67549ba465325d7c58ac9903172edbb2482
jfo <jfo@pfb20.cshl.edu>
2018-10-17 18:33:45 -0400

    Added PS4-6 with sorted numbers

--- Python_04/PS4-6.py
+++ Python_04/PS4-6.py
@@ -2,8 +2,14 @@
 
 numlist = ['101','2','15','22','95','33','2','27','72','15','52']
 
-for num in numlist:
-	if int(num)%2 == 0:
-		print(num)
+#for num in numlist:
+#	if int(num)%2 == 0:
+#		print(num)
+
+
+sortNum = sorted(numlist)
+print(sortNum)
+
+
 print("Done!")
 
