7c6a29ee6348103ddb775b4a8fae0d9fe007f2fe
jfo <jfo@pfb20.cshl.edu>
2018-10-17 16:55:16 -0400

    While loop for real

--- Python_04/PS4-5.py
+++ Python_04/PS4-5.py
@@ -3,9 +3,9 @@
 #Calculate factorial of 1000
 count = 1
 factorial = 1
-while count < 6:
+while count < 10001:
 	factorial *= count 
 	count+=1
-print(factorial)
+print("Factorial of 1000 =",factorial)
 print("All done!")
 
