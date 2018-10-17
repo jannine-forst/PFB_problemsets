926efcdc65729c5cd2b7f1f6ed489dbbb88ff40b
jfo <jfo@pfb20.cshl.edu>
2018-10-17 11:49:17 -0400

    Added substring extraction

---
+++ Python_03/PS3-10.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+
+dna = sys.argv[1]
+
+capitalDNA = dna.upper()
+
+numA = capitalDNA.count("A")
+numT = capitalDNA.count("T")
+numG = capitalDNA.count("G")
+numC = capitalDNA.count("C")
+
+
+#countAT = numA + numT
+#countGC = numG + numC
+#countTotal = numA + numT + numG + numC
+
+#contentAT = countAT/countTotal
+#contentGC = countGC/countTotal
+
+
+#print("AT Content:",contentAT)
+#print("GC Content:",contentGC)
+
+subDNA = capitalDNA[99:199]
+print("Length of DNA subsequence:",len(subDNA))
+print("DNA subsequence:"subDNA)
+
+
