a8743591924a0069fda7aeb1c202fe253d0926ad
jfo <jfo@pfb20.cshl.edu>
2018-10-17 13:16:35 -0400

    Up to PS3-13

--- Python_03/PS3-13.py
+++ Python_03/PS3-13.py
@@ -11,7 +11,12 @@ complAT = complA.replace('t','A')
 complATG = complAT.replace('g','C')
 complATGC = complATG.replace('c','G')
 
-print("This is the complement: 3'",complATGC,"5'")
+#reverse the complement
+revDNA = complATGC[::-1]
 
+#Print out the answers
+print("Original Sequence\t5'",dna,"5'")
+print("Complement\t\t3'",complATGC,"5'")
+print("Reverse Complement\t5'",revDNA,"3'")
 
 
