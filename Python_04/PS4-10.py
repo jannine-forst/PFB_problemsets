2d265d6bb7f0fde23ca7bf4fd2ed73a784ca0933
jfo <jfo@pfb20.cshl.edu>
2018-10-18 08:41:01 -0400

    Added PS4-11

--- Python_04/PS4-10.py
+++ Python_04/PS4-10.py
@@ -4,8 +4,10 @@ import sys
 start=int(sys.argv[1])
 end=int(sys.argv[2])
 
-if
-[print(x) for x in range(start,end)]
+for x in range(start,end):
+	if x%2 != 0:
+		print(x)
+
 
 print("Done!")
 
---
+++ Python_04/PS4-11.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+seq = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+
+for each in seq:
+	print(each)
