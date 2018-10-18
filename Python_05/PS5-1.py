a936c7c9448de4b58b2e070989b81a16a697bd51
jfo <jfo@pfb20.cshl.edu>
2018-10-17 20:20:10 -0400

    Extended PS5-1

--- Python_05/PS5-1.py
+++ Python_05/PS5-1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+#Make dictionary, print fav book
 fav = {'book':'Sabriel','song':'the killers','tree':'maple'}
 print(fav['book'])
+
+#Print out fav book by using variable
+favbook = 'book'
+print(fav(favbook))
 
 
