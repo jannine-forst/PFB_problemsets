9a9a31d406fb904f39a2365fe549ddf744cc4b34
jfo <jfo@pfb20.cshl.edu>
2018-10-17 22:16:22 -0400

    Added PS4-10

---
+++ Python_04/PS4-10.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+start=int(sys.argv[1])
+end=int(sys.argv[2])
+
+if
+[print(x) for x in range(start,end)]
+
+print("Done!")
+
--- Python_04/PS4-9.py
+++ Python_04/PS4-9.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-print(x) for x in range(1,100)
+[print(x) for x in range(1,100)]
 
 print("Done!")
 
