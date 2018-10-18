8c799f9998c29e514d08a152aa3a7e649a8c533b
jfo <jfo@pfb20.cshl.edu>
2018-10-17 21:07:14 -0400

    Extended PS5-10

---
+++ Python_05/PS5-10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+setA={3,14,15,9,26,5,35,9}
+setB={60,22,14,0,9}
+
+#Difference
+print("The differences are:",setA-setB)
+
+#Union
+print("The union is:",setA|setB)
+
+#Symmetrical difference
+print("The symmetrical difference is:",setA^setB)
+
+setC={sys.argv[1]}
+
+print(setC)
+
