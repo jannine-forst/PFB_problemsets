52797692a3eeb4cb1b36dd03cae9c0ef00095c82
jfo <jfo@pfb20.cshl.edu>
2018-10-18 08:57:53 -0400

    Added problem 12 into PS4-11

--- Python_04/PS4-11.py
+++ Python_04/PS4-11.py
@@ -2,5 +2,19 @@
 
 seq = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
 
-for each in seq:
-	print(each)
+
+#Problem 11 Part 1 - print each item in list using for loop
+#for each in seq:
+#	print(each)
+
+#Problem 11 Part 2 - print each item with its length and sequence, tab separated
+#for each in seq:
+#	print(len(each),"\t",each)
+
+
+#Problem 12 - use list comprehension
+tuple_list = (print(len(each),"\t",each,"\n") for each in seq)
+print(type(tuple_list))
+
+
+
