072a2d2f54d9881db4e67e32e7b37a236b51d029
jfo <jfo@pfb20.cshl.edu>
2018-10-21 11:48:09 -0400

    More added to PS10

--- PS10-1.py
+++ PS10-1.py
@@ -2,7 +2,19 @@
 
 class DNArecord(object):
 	def __init__(self,gene_name,DNA_sequence,gene_organism):
-		self.gene_bname = gene_name
+		self.gene_name = gene_name
 		self.DNA_sequence = DNA_sequence
 		self.gene_organism = gene_organism
+
+	def seq_length(self):
+		seq_length = len(self.DNA_sequence)
+		return seq_length
+
+
+dna_record_obj1 = DNArecord('AGTCAGTC','ABC1','Mycobacterium tuberculosis')
+
+for d in [dna_record_obj1]:
+	print('name:', d.gene_name, '\t', 'seq:',d.DNA_sequence)
+
+
 
