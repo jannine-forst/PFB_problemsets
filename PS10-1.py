99677c542a53e9ad1a3265676c192eeaa8b369e7
jfo <jfo@pfb20.cshl.edu>
2018-10-21 13:15:44 -0400

    Done up to challenge of PS10

--- PS10-1.py
+++ PS10-1.py
@@ -19,15 +19,28 @@ class DNArecord(object):
 		nucl_content = ('A: '+'{:.2%}'.format(contentA)+'\n'+'G: '+'{:.2%}'.format(contentG)+'\n'+'C: '+'{:.2%}'.format(contentC)+'\n'+'T: '+'{:.2%}'.format(contentT)+'\n')
 		return nucl_content  
 
+	def gc_content(self):
+		length = len(self.DNA_sequence)
+		compG = self.DNA_sequence.count('G')
+		compC = self.DNA_sequence.count('C')
+		gc_content = (compG + compC) / length
+		return gc_content
+
+	def fasta_formatter(self):
+		fasta = ">{} | {}\n{}".format(self.gene_name, self.gene_organism, self.DNA_sequence)
+		return fasta
+
+				
+
 
 
 dna_record_obj1 = DNArecord('AGTCAGTC','ABC1','Mycobacterium tuberculosis')
 
 for d in [dna_record_obj1]:
 	print('name:', d.gene_name, '\t', 'seq:',d.DNA_sequence)
-	print('length:',d.seq_length())
-	print(d.nucl_comp())
-	print("Expect 25% for each")
-
-
+	print('\nlength:',d.seq_length())
+	print("\nNucleotide composition:\n"+d.nucl_comp())
+	#print("Expect 25% for each")
+	print("GC content: ","{:.2%}".format(d.gc_content()))
+	print("\n"+d.fasta_formatter())
 
